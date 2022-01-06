a0fa75c86715cb8106d15ce9cd5570946ea6ad75
sawich <sawich@angeldev.org>
2022-01-06 12:00:15 +0200

    log lua_atprint when used

--- lua100/sources/exports.cpp
+++ lua100/sources/exports.cpp
@@ -1,6 +1,7 @@
-
 #include "../headers/exports.hpp"
+#include <spdlog/spdlog.h>
 
- void lua_atprint([[maybe_unused]] lua_State* _state, [[maybe_unused]] uintptr_t _unk_ptr)
- {
- }
+void lua_atprint([[maybe_unused]] lua_State* _state, [[maybe_unused]] uintptr_t _unk_ptr)
+{
+  spdlog::warn("lua_atprint called");
+}
