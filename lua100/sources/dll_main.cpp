153a0355a2bf7a4f652174a1496f4541038de526
sawich <sawich@angeldev.org>
2022-04-12 05:50:32 +0300

    add configuration file

--- lua100/sources/dll_main.cpp
+++ lua100/sources/dll_main.cpp
@@ -20,14 +20,30 @@
 
 auto APIENTRY DllMain(HMODULE _module, DWORD _call_reason, [[maybe_unused]] LPVOID _reserved) -> BOOL
 {
+  namespace slevel = spdlog::level;
+  namespace fs = std::filesystem;
+
+#ifdef _DEBUG
+  std::string log_level{ SPDLOG_LEVEL_NAME_DEBUG };
+  bool log_console = true;
+#else
+  std::string log_level{ SPDLOG_LEVEL_NAME_OFF };
+  bool log_console = false;
+#endif // _DEBUG
+
   switch (_call_reason)
   {
   case DLL_PROCESS_ATTACH: {
     DisableThreadLibraryCalls(_module);
-    AllocConsole();
+
+    auto&& config{ fs::exists("configs/loader.yml") ? toml::parse_file("configs/loader.yml") : toml::parse_result{} };
+    
+    if (config["logger"]["use_console"].value_or<bool>(std::forward<bool>(log_console))) {
+      AllocConsole();
+    }
 
     spdlog::set_default_logger(spdlog::stdout_color_mt<spdlog::async_factory>("loader", spdlog::color_mode::always));
-    spdlog::set_level(spdlog::level::debug);
+    spdlog::set_level(slevel::from_str(config["logger"]["level"].value_or<std::string>(std::forward<std::string>(log_level))));
 
     std::thread{ &lua100::plugins::attach, &lua100::plugins::instance }.detach();
 
