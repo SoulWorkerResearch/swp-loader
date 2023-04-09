02f7ecbd6854e70a62c526941bc773dcd4f47912
sawich <sawich@angeldev.org>
2023-04-09 07:31:49 +0300

    🔥 chore(app.hpp, exports.hpp, logger_factory.hpp): delete unused files
    🗑️ chore(pch.hpp, plugin_info.hpp): delete pch.hpp and plugin_info.hpp files
    
    🔥 chore(plugins.hpp, config.hpp, game_version.hpp, logger.hpp): delete unused files
    
    🔥 chore(plugin_loader.hpp, dll.hpp): remove unused files

--- lua100/headers/utils/game_version.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-namespace lua100::utils::game_version
-{
-  auto read(void)->swpsdk::version;
-}
--- lua100/headers/utils/logger.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace lua100::utils::logger
-{
-  auto pack(const std::string& _path) -> void;
-
-  auto system_error(void) -> void;
-}
