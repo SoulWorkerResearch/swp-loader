c90d39ce5dada82146c9ffe1972249131a3c3767
sawich <sawich@angeldev.org>
2023-04-09 07:31:10 +0300

    🗑️ chore(UpdateVersion.ps1): delete UpdateVersion.ps1 script file
    🗑️ chore(deploy.yml): remove deploy workflow file.

--- .github/scripts/UpdateVersion.ps1
+++
@@ -1,8 +0,0 @@
-$version = $Env:VERSION -replace '\.',', ' -replace '-',', '
-
-$path =  '../../swp-sdk/headers/swpsdk/defines.hpp'
-$content = Get-Content -Path $path
-
-$content = $content -replace '(.*?current_version{) .*',('$1 ' + $version + ' };')
-
-Set-Content -Path $path -Value $content
